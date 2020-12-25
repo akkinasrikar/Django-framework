7834726ad4814330325e3d18c2c017f2c6714c54
akkinasrikar <akkinasrikar@gmail.com>
2020-12-25 10:53:58 +0530

    Commited

--- Django Level Five/learning_users/basic_app/models.py
+++ Django Level Five/learning_users/basic_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class UserProfileInfo(models.Model):
 
-	user=models.OneToOneField(User)
+	user=models.OneToOneField(User,on_delete=models.CASCADE)
 
 	portfolio_site = models.URLField(blank=True)
 	profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
